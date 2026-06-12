445bca8e6e6056f4c1c53285368df7ee4c3c336f
mrinal062004 <tiwari.mrinal2004@gmail.com>
2026-06-12 13:43:19 +0530

    Model training completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Kidney import logger
 from Kidney.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 from Kidney.pipeline.stage02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from Kidney.pipeline.stage03_training_model import ModelTrainingPipeline
 STAGE_NAME="Data Ingestion stage"
 
 try:
@@ -22,3 +23,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME="Model Training"
+try:
+    logger.info(f"_______________________________________")
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    model_trainer=ModelTrainingPipeline()
+    model_trainer.main()
+    logger.info(f"stage {STAGE_NAME} completed <<<<< \n\n X---------------------")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
