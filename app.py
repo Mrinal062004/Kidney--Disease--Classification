0711a451ad69bf8a01ff09d7bd06ad489f6eaf27
mrinal062004 <tiwari.mrinal2004@gmail.com>
2026-06-16 12:24:43 +0530

    corrected

--- app.py
+++ app.py
@@ -12,10 +12,11 @@ app =Flask(__name__)
 CORS(app)
 class ClientApp:
     def __init__(self):
-        self.filename="inputImage.jpg"
-        self.classifier=PredictionPipeline(self.filename)
+        self.filename = "inputImage.jpg"
 
-
+        self.classifier = PredictionPipeline(
+    "artifacts/training/model.h5"
+)
 
 @app.route("/",methods=["GET"])
 @cross_origin()
